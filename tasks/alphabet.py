ce865df63d718dc303e68a3b4646d42f49462e4d
DarkWell005 <darkwell463@gmail.com>
2024-07-27 14:12:15 +0300

    Решение задач: - Alphabet

--- tasks/alphabet.py
+++ tasks/alphabet.py
@@ -1,3 +1,35 @@
-# TODO: 1. Алфавит
+import string
 
-# https://smartiqa.ru/courses/python/lesson-6#hometask
+
+class Alphabet:
+    def __init__(self, lang: str, letters: str):
+        self.lang = lang
+        self.letters = letters
+        print(letters)
+
+    def letters_num(self):
+        return len(self.letters)
+
+
+def example():
+    return "HELLO"
+
+
+class EngAlphabet(Alphabet):
+    def __init__(self):
+        super().__init__("En", string.ascii_uppercase)
+
+    __letters_num = len(string.ascii_uppercase)
+
+    def is_en_letter(self, letter):
+        return letter in self.letters
+
+    def letters_num(self):
+        return self.__letters_num
+
+
+eng = EngAlphabet()
+print(eng.letters_num())
+print(eng.is_en_letter("F"))
+print(eng.is_en_letter("Щ"))
+print(example())
