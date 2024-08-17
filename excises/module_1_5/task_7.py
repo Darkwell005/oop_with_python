100d8d0e73f704a76078823ef91e34409b2969b2
DarkWell005 <darkwell463@gmail.com>
2024-08-17 11:58:25 +0300

    Решение задач: - Модуль 1.5 -> №7

--- excises/module_1_5/task_7.py
+++ excises/module_1_5/task_7.py
@@ -1,16 +1,16 @@
 class CPU:
-    def __init__(self, name, fr):
+    def __init__(self, name: str, fr: int):
         self.name = name
-        self.fps = fr
+        self.fr = fr
+
+    def __str__(self):
+        return ", ".join([str(x) for x in [self.name, self.fr]])
 
 
 class Memory:
-    def __init__(self, name, volume):
+    def __init__(self, name: str, volume: int):
         self.name = name
         self.volume = volume
-
-    def __str__(self):
-        return f'{self.name} - {self.volume}'
 
 
 class MotherBoard:
@@ -20,11 +20,23 @@ class MotherBoard:
         self.__total_mem_slots = 4
         self.mem_slots: list['Memory'] = list(mem_slots[:self.__total_mem_slots])
 
-        # print('Память: ', end='')
-        # for item in self.mem_slots:
-        #     print(item.name, item.volume, sep=' - ', end='; ')
-        #
-        # '; '.join(f"{item.name} - {item.volume}" for item in self.mem_slots)
+    def _some_method(self) -> str:
+        return '; '.join(f"{item.name} - {item.volume}" for item in self.mem_slots)
 
-    def get_config(self):
-        return
+    def get_config(self) -> str:
+        return (f"Материнская плата: {self.name}, "
+                f"\nЦентральный процессор: {self.cpu}, "
+                f"\nСлотов памяти: {self.__total_mem_slots}, "
+                f"\nПамять: {self._some_method()}"
+
+                )
+
+
+cp = CPU("Intel Core i5", 3000)
+mem = Memory("SIMM", 512)
+mem_2 = Memory("DIMM", 1024)
+mem_3 = Memory("FB-DIMM", 256)
+mem_4 = Memory("SODIMM", 128)
+mem_5 = Memory("RIMM", 64)
+mb = MotherBoard("Micro-ATX", cp, mem, mem_2, mem_3, mem_4, mem_5)
+print(mb.get_config())
