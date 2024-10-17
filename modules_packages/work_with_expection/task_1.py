a4a42a5f0650b1736dd844bc919f7f52cddaa2b9
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-10-17 12:21:04 +0100

    8. Исключения
    - Задание №1

--- modules_packages/work_with_expection/task_1.py
+++ modules_packages/work_with_expection/task_1.py
@@ -1,9 +1,47 @@
-for _ in range(4):
-    try:
-        user_input = int(input())
-        if 10 < user_input <= 20:
-            print(f"Работаем с числом {user_input}")
+# INFO: Согласно условию задачи, Вам необходимо написать функцию.
+# Приведу два возможных решения
+
+def get_num_from_user() -> int:
+    num: str = input("Введите число: ")
+
+    start: int = 10
+    end: int = 20
+    if (not num.isdigit()) or (not (start <= int(num) <= end)):
+        raise ValueError(
+            "Ошибка! Вы ввели не число или число не находится в диапазоне!"
+        )
+
+    return int(num)
 
 
-    except ValueError:
-        print("Ошибка! Вы ввели не число или число не находится в диапазоне!")
+# Можно сделать код ещё читабельнее
+def validate_num(num: str):
+    start: int = 10
+    end: int = 20
+    if (not num.isdigit()) or (not (start <= int(num) <= end)):
+        raise ValueError(
+            "Ошибка! Вы ввели не число или число не находится в диапазоне!"
+        )
+
+
+def get_num_from_user_short() -> int:
+    num: str = input("Введите число: ")
+    validate_num(num)
+
+    return int(num)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            number: int = get_num_from_user()
+        except ValueError as error_msg:
+            print(error_msg)
+        except Exception as error_msg:
+            print(f"Неожиданная ошибка: {error_msg}")
+        else:
+            print(f"Работаем с числом {number}")
+            break
+
+# Ещё в задаче сказано "Максимальная длина кода тела функции 4 строки:"
+# не обращайте на это внимание, пишите читаемый и чистый код, вместо короткого
